c4eb6aeccf072ac598948fcc9f0371df2cd003f6
Isaac Willian <isaaacwillian@gmail.com>
2022-05-09 01:13:40 -0300

    Added pulse to the loader

--- src/components/Loader/style.ts
+++ src/components/Loader/style.ts
@@ -1,10 +1,22 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
+const pulse = keyframes`
+0% {
+    transform: scale(1);
+    filter: brightness(100%);
+  }
+  100% {
+    transform: scale(1.1);
+    filter: brightness(200%);
+  }
+`;
 export const Container = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
   height: 100vh;
+  animation: ${pulse} 0.7s infinite;
+  animation-direction: alternate;
   #logo {
     img {
       width: calc(15vh + 6vw);
